aaa0819673df7e7ad3235f1dfa333a3aaac1d9c2
TheDjAlem <TheDjAlem@users.noreply.github.com>
2024-06-06 19:26:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="TheDjAlem"
+original_name="api2"
+original_urlname="api2"
+original_description="Awesome api2 created by TheDjAlem"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv api2 $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
